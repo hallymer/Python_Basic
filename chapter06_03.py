c59378a8790084bd1a7dd80b7885199c04070a38
hallymer <mu07268@gmail.com>
2020-07-02 01:07:49 +0900

    Python 파이썬 파이썬 클래스 및 모듈, 패키지_03 파일
    패키지(Package)개념
    패키지 구조 설명, __init__.py, 패키지 함수 실행, 패키지 경로, 패키지 사용 실습에 대해 배웠다.
    
    패키지  : 모듈의 분활된 개별적인 모듈이 모여 하나의 패키지가 되는것 즉, 쉽게 말해 모듈을 모아놓은 폴더
    
    모듈화흘 시키는 것은 중요하다!
    
    모듈을 import할 때는
    풀네임으로 하는 건 잘 사용하지 않는다.
    
    from절을 통해서 정확히 패키지의 depth 경로를 찾아가서
    내가 사용하고 싶은 모듈만 정확하게 import를 하고 alias까지 준다하면 간단하게 사용할 수 있다!
    
    *(별표)는 웬만하면 사용하지말자!
    파이썬이 실행 될 때 모든 모듈을 다 사용하는 것이 아니기 때문
    굳이 불 필요한 작업을 할 필요가 없다! 누적이되면 메모리를 잡아먹어 성능저하가 될 수 있다!
    
    pycache라는 것 런타임 환경 때 파일의 빠른 실행을 위해서 파이썬 엔진이 만드는 것 삭제해도 상관이 없다!
    
    __init__.py : Python3.3 부터는 없어도 패키지로 인식 -> 단, 하위 호환을 위해 작성 추천

--- chapter06_03.py
+++ chapter06_03.py
@@ -2,6 +2,7 @@
 # 파이썬 패키지
 # 패키지 작성 및 사용법
 # 파이썬은 패키지로 분할된 개별적인 모듈로 구성
+# __init__.py : Python3.3 부터는 없어도 패키지로 인식 -> 단, 하위 호환을 위해 작성 추천
 # 상대경로 : .. (부모 디렉토리), .(현재 디렉토) --> 모듈 내부에서만 사용
 
 # 예제1
@@ -20,3 +21,36 @@ print()
 print()
 
 # 예제2
+from sub.sub1 import module1
+from sub.sub2 import module2 as m2 # as --> alias (별명)
+
+# 사용
+module1.mod1_test1()
+module1.mod1_test2()
+
+# 사용
+m2.mod2_test1()
+m2.mod2_test2()
+
+print()
+print()
+print()
+
+# 예제3
+from sub.sub1 import *
+from sub.sub2 import *
+# *(별표)는 웬만하면 사용하지말자!
+# 파이썬이 실행 될 때 모든 모듈을 다 사용하는 것이 아니기 때문
+# 굳이 불 필요한 작업을 할 필요가 없다! 누적이되면 메모리를 잡아먹어 성능저하가 될 수 있다!
+
+# 사용
+module1.mod1_test1()
+module1.mod1_test2()
+
+# 사용
+module2.mod2_test1()
+module2.mod2_test2()
+
+print()
+print()
+print()
